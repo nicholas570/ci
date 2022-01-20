de678e9e310174c6cf34f2d23ce6dfba07b40962
nicolas <nmercier@d-edge.com>
2022-01-20 17:23:43 +0100

    ci: test job

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/no no! no no no no!/i);
   expect(linkElement).toBeInTheDocument();
 });
